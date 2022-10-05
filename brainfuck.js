439e84315a0309051c2dbcb4f4bf7e533f609bc5
Sid Shardanand <51441497+s1ddly@users.noreply.github.com>
2022-10-05 23:16:32 +1100

    First non readme commit lmao

---
+++ brainfuck.js
@@ -0,0 +1,11 @@
+/*
+JS file for the brainfuck interpreter
+Usage:
+1. Import the js file into your html page
+2. Call the brainfuck function with the 2 arguments
+	2a. Argument 1 is codeblock, this is a block of brainfuck code to execute, in string format.
+	2b. Argument 2 is the input for this codeblock, so far as a string, but will expand on this as the project evolves
+*/
+function brainfuck(codeblock, input){
+	
+}

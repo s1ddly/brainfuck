fd741ab39d6511db116d440469475124a9deb098
Sid Shardanand <51441497+s1ddly@users.noreply.github.com>
2022-10-09 14:12:48 +1100

    Fleshing out error messages

--- brainfuck.js
+++ brainfuck.js
@@ -4,21 +4,23 @@ Usage:
 1. Import the js file into your html page
 2. Call the brainfuck function with the 2 arguments
 	2a. Argument 1 is codeblock, this is a block of brainfuck code to execute, in string format.
-	2b. Argument 2 is the input for this codeblock, so far as a string, but will expand on this as the project evolves
+	2b. Argument 2 is the input for this codeblock, so far as a multi line string consisting of 1 byte unsigned ints(0-255) separated by new line characters, but will expand on this as the project evolves
 */
 var bfmem = Array(30000).fill(0);
 var bfind = 0;
+var bfout = [];
 
 function brainfuck(bfcode, bfinput){
 	bfstdin = bfinput.split("\n");
 	for(var bfcodeind = 0; bfcodeind++; bfcodeind < bfcode.length){
 		
 	}
+	console.log(bfout);
 }
 
 function bfincrement(){
 	if(bfmem[bfind] == 255){
-		console.log("Memory value overflow, setting memory index to 0");
+		console.log("WARN: Memory value overflow, setting memory index to 0");
 		bfmem[bfind] = 0;
 	} else {
 		bfmem[bfind] = bfmem[bfind] + 1;
@@ -27,7 +29,7 @@ function bfincrement(){
 
 function bfdecrement(){
 	if(bfmem[bfind] == 0){
-		console.log("Memory value underflow, setting memory index to 255");
+		console.log("WARN: Memory value underflow, setting memory index to 255");
 		bfmem[bfind] = 255;
 	} else {
 		bfmem[bfind] = bfmem[bfind] - 1;
@@ -36,7 +38,7 @@ function bfdecrement(){
 
 function bfright(){
 	if(bfind == 29999){
-		console.log("Memory reference overflow, setting memory index to 0");
+		console.log("WARN: Memory reference overflow, setting memory index to 0");
 		bfind = 0;
 	} else {
 		bfind = bfind + 1;
@@ -45,7 +47,7 @@ function bfright(){
 
 function bfleft(){
 	if(bfind == 0){
-		console.log("Memory reference underflow, setting memory index to 29999");
+		console.log("WARN: Memory reference underflow, setting memory index to 29999");
 		bfind = 29999;
 	} else {
 		bfind = bfind - 1;
@@ -57,14 +59,19 @@ function bfinput(){
 		bfmem[bfind] = parseInt(bfstdin[0]);
 		bfstdin.shift();
 	} else {
-		console.log("Error on char: " + bfcodeind.toString() + "\n\tCodeblock asked for input but none was provided");
+		console.log("ERROR: Codeblock asked for input on char: " + bfcodeind.toString() + ", but none was provided");
+		throw "Input Error";
 	}
 }
 
 function bfoutput(){
-	
+	bfout.push(bfmem[bfind]);
 }
 
 function bfloop(){
+	
+}
+
+function bfoutput(){
 	
 }
